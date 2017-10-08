1e017252b79598d2234f027f1c1a8f6b9d15a1e7
Dan Moore <bitbucket@mooreds.com>
2017-10-08 16:34:22 +0000

    more debugging

--- lib/slowpoke/middleware.rb
+++ lib/slowpoke/middleware.rb
@@ -7,12 +7,16 @@ module Slowpoke
     def call(env)
       @app.call(env)
     ensure
+      puts "in ensure"
+      puts "envkey: "
+      puts env[Slowpoke::ENV_KEY]
       if env[Slowpoke::ENV_KEY]
         # extremely important
         # protect the process with a restart
         # https://github.com/heroku/rack-timeout/issues/39
         # can't do in timed_out state consistently
         if defined?(::PhusionPassenger)
+          puts "in phusionpassenger"
           `passenger-config detach-process #{Process.pid}`
         else
           Process.kill("QUIT", Process.pid)
